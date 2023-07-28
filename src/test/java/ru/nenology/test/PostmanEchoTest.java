fe7b9ee990a78553ce3a257d518ad9b63de0607c
Vladimir444k <gluschenkovladimir337@gmail.com>
2023-07-28 11:48:41 +0300

    test comm

--- src/test/java/ru/nenology/test/PostmanEchoTest.java
+++ src/test/java/ru/nenology/test/PostmanEchoTest.java
@@ -15,7 +15,7 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hi"))
+                .body("data", equalTo("HI"))
         ;
     }
 }
